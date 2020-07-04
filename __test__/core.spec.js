c25c2c5dc002b3d3528ce315ac50af19fc80f9be
mfeyx <github@feyx.de>
2020-07-04 22:57:36 +0200

    added more core tests

--- __test__/core.spec.js
+++ __test__/core.spec.js
@@ -1,7 +1,7 @@
 const {
   type,
-  // unveil,
-  // unveilType
+  unveil,
+  unveilType
 } = require('../src/core')
 
 describe('core.type()', () => {
@@ -14,9 +14,30 @@ describe('core.type()', () => {
   test('1.1 to be a Float', () => {
     expect(type(1.1)).toBe('Float')
   })
+  test('"Hello" to be a String', () => {
+    expect(type('Hello')).toBe('String')
+  })
   test.todo('other types')
 })
-describe('Todo', () => {
-  test.todo('core.unveil()')
-  test.todo('core.unveilType()')
+
+describe('core.unveil()', () => {
+  test('"[1, 2]" should become [1, 2]', () => {
+    expect(unveil('[1, 2]')).toEqual([1, 2])
+  })
+  test('"1" should become 1', () => {
+    expect(unveil('1')).toBe(1)
+  })
+  test('"1.1" should become 1.1', () => {
+    expect(unveil('1.1')).toBe(1.1)
+  })
+  test.todo('"{a: 1}" to become {a: 1}')
+})
+
+describe('core.unveilType()', () => {
+  test('"1.1" should become 1.1 and be Type Float', () => {
+    expect(unveilType('1.1')).toBe('Float')
+  })
+  test('"1" should become 1 and be Type Integer', () => {
+    expect(unveilType('1')).toBe('Integer')
+  })
 })
