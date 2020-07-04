a7f46c07f3812d576eefbccf6b130380eabca334
mfeyx <github@feyx.de>
2020-07-04 22:49:59 +0200

    snitch tests

--- __test__/snitch.spec.js
+++ __test__/snitch.spec.js
@@ -4,4 +4,18 @@ describe('Testing Snitch API', () => {
   test('snitch should be an object', () => {
     expect(typeof snitch).toEqual(typeof {})
   })
+  test('snitch should have type method', () => {
+    expect(snitch.type).toBeDefined()
+    expect(snitch.type).not.toBeNull()
+  })
+  test('snitch.type should be a Function', () => {
+    expect(typeof snitch.type).toEqual(typeof Function)
+  })
+  test('snitch should have unveil method', () => {
+    expect(snitch.unveil).toBeDefined()
+    expect(snitch.unveil).not.toBeNull()
+  })
+  test('snitch.unveil should be a Function', () => {
+    expect(typeof snitch.unveil).toEqual(typeof Function)
+  })
 })
