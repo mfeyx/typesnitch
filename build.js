483fae37f37635c6c869a01213e5ffbde16eb96e
mfeyx <github@feyx.de>
2020-07-05 19:53:31 +0200

    build script

--- build.js
+++ build.js
@@ -2,13 +2,25 @@ const shell = require('shelljs')
 const pkg = require('./package')
 
 
-shell.echo('Start Bulding Package...')
+console.log('Running Tests')
+shell.exec('npm test')
+
+console.log('Running Lint')
+shell.exec('npm run lint')
+
+console.log('Start Bulding Package...')
+shell.exec('rm -rf dist')
 shell.exec('rm -rf dist')
 shell.exec('mkdir -p dist')
+
+
+console.log('Copy files into dist folder...')
 shell.exec('cp -r src/snitch.js dist')
 shell.exec('cp -r src/utils dist')
 shell.exec('cp LICENSE dist')
 shell.exec('cp typesnitch.package.json dist/package.json')
 shell.exec(`sed -i -- 's/VERSION/${pkg.version}/g' dist/package.json`)
 shell.exec('rm dist/package.json--')
-// shell.exec('npm run publish dist --dry-run')
+
+console.log('Publish package to npm...')
+// shell.exec('npm publish dist --dry-run')
