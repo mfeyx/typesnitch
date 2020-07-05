830fb8dba57315f73c554d43da04194f14e13ea0
mfeyx <github@feyx.de>
2020-07-05 22:37:21 +0200

    performance

--- performance.js
+++ performance.js
@@ -1,7 +1,7 @@
 const timeit = require('timeit-js')
 const benchmark = require('benchmark')
 
-const snitch = require('./src/snitch')
+const { type } = require('./src/snitch')
 const kindof = require('kind-of')
 
 const x = false
@@ -10,12 +10,12 @@ const options = { e: 10000000, r: 10, d: 10 }
 timeit(kindof, x, options)
 console.log('\n')
 
-timeit(snitch.type, x, options)
+timeit(type, x, options)
 console.log('\n')
 
 const suite = new benchmark.Suite
 suite
-  .add('snitch => str', () => snitch.type(x) )
+  .add('snitch => str', () => type(x) )
   .add('kindof => str', () => kindof(x))
   .on('cycle', function (event) { console.log(String(event.target)) })
   .on('complete', function () { console.log('Fastest is ' + this.filter('fastest').map('name')) })
