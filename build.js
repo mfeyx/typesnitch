acb53d34f53bd6d5a7af4ab8a33a86f347f9302e
mfeyx <github@feyx.de>
2020-07-05 19:59:52 +0200

    build script

--- build.js
+++ build.js
@@ -1,7 +1,6 @@
 const shell = require('shelljs')
 const pkg = require('./package')
 
-
 console.log('Running Tests')
 shell.exec('npm test')
 
@@ -13,11 +12,11 @@ shell.exec('rm -rf dist')
 shell.exec('rm -rf dist')
 shell.exec('mkdir -p dist')
 
-
 console.log('Copy files into dist folder...')
 shell.exec('cp -r src/snitch.js dist')
 shell.exec('cp -r src/utils dist')
 shell.exec('cp LICENSE dist')
+shell.exec('cp REAMDME.md dist')
 shell.exec('cp typesnitch.package.json dist/package.json')
 shell.exec(`sed -i -- 's/VERSION/${pkg.version}/g' dist/package.json`)
 shell.exec('rm dist/package.json--')
