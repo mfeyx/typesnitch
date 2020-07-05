f40465091d98b797bd3072e92f43611a1d10347f
mfeyx <github@feyx.de>
2020-07-05 21:04:23 +0200

    changed getPrototype function

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,7 +6,53 @@
  * @return {string}
  */
 function getPrototype (obj) {
-  return Object.prototype.toString.call(obj).slice(8, -1)
+
+  /** @private */
+  function _getConstructorString (obj) {
+    return obj.constructor.toString()
+  }
+  /** @private */
+  function _getPrototypeString (obj) {
+    return Object.prototype.toString.call(obj)
+  }
+
+  const type = _getPrototypeString(obj)
+  if (type === '[object Uint8Array]') {
+    return _getConstructorString(obj).includes('Buffer')
+      ? 'Buffer'
+      : 'Uint8Array'
+  } else {
+    return {
+      '[object Object]': 'Object',
+      '[object Array]': 'Array',
+      '[object String]': 'String',
+      '[object Boolean]': 'Boolean',
+      '[object Number]': 'Number',
+      '[object Undefined]': 'Undefined',
+      '[object RegExp]': 'RegExp',
+      '[object Null]': 'Null',
+      '[object Uint16Array]': 'Uint16Array',
+      '[object Uint32Array]': 'Uint32Array',
+      '[object ArrayBuffer]': 'ArrayBuffer',
+      '[object Function]': 'Function',
+      '[object Date]': 'Date',
+      '[object Arguments]': 'Arguments',
+      '[object Error]': 'Error',
+      '[object GeneratorFunction]': 'GeneratorFunction',
+      '[object Symbol]': 'Symbol',
+      '[object Map]': 'Map',
+      '[object WeakMap]': 'WeakMap',
+      '[object Set]': 'Set',
+      '[object Int8Array]': 'Int8Array',
+      '[object Int16Array]': 'Int16Array',
+      '[object Int32Array]': 'Int32Array',
+      '[object WeakSet]': 'WeakSet',
+      '[object BigInt]': 'BigInt',
+      '[object Float32Array]': 'Float32Array',
+      '[object Float64Array]': 'Float64Array',
+      '[object Uint8ClampedArray]': 'Uint8ClampedArray'
+    }[type]
+  }
 }
 
 /**
