a8b7eab84d88704d94ab64fac7a27597bc9a8e67
mfeyx <github@feyx.de>
2020-07-12 15:28:22 +0200

    refactoring

--- src/detect/index.js
+++ src/detect/index.js
@@ -1,36 +1,36 @@
 const isNull = value => value === null
 const isUndefined = value => value === undefined
+
+const isBoolean = value => typeof value === 'boolean'
 const isString = value => typeof value === 'string'
-const isNumber = value => typeof value === 'number'
-const isNaN = value => typeof value === 'number' && Number.isNaN(1 / +value)
-const isArray = value => Array.isArray(value)
 const isBigInt = value => typeof value === 'bigint'
 const isSymbol = value => typeof value === 'symbol'
-const isBuffer = value => typeof value === 'object' && value.constructor.name === 'Buffer'
+
+const isArray = value => Array.isArray(value)
+
 const isDate = value => value instanceof Date
 const isError = value => value instanceof Error
 const isRegExp = value => value instanceof RegExp
-const isObject = value => typeof value === 'object' && value.constructor.name === 'Object'
-const isBoolean = value => typeof value === 'boolean'
-const isFloat = value => typeof value === 'number' && !Number.isNaN(value) && `${value}`.indexOf('.') !== -1
-const isInteger = value => Number.isInteger(value)
-const isBaseObject = value => typeof value === 'object'
+
+const isNumber = value => typeof value === 'number'
+const isNaN = value => isNumber(value) && Number.isNaN(1 / +value)
+const isInteger = value => isNumber(value) && Number.isInteger(value)
+const isFloat = value => isNumber(value) && !Number.isNaN(value) && `${value}`.indexOf('.') !== -1
 const isNegInfinity = value => isNumber(value) && !isInteger(value) && !isFloat(value) && !isNaN(value) && value < 0
 const isPosInfinity = value => isNumber(value) && !isInteger(value) && !isFloat(value) && !isNaN(value) && value > 0
-const isFunction = value => typeof value === 'function'
-const isGenerator = value => typeof value === 'function' && value.constructor.name === 'GeneratorFunction'
-const isUint16Array = value => typeof value === 'object' && value.BYTES_PER_ELEMENT === 2
-const isUint32Array = value => typeof value === 'object' && value.BYTES_PER_ELEMENT === 4
-const isBigInt64Array = value => typeof value === 'object' && value.BYTES_PER_ELEMENT === 8
-const isUint8Array = value => typeof value === 'object' && value.constructor.name === 'Uint8Array'
-const isUint8ClampedArray = value => typeof value === 'object' && value.constructor.name === 'Uint8ClampedArray'
+const isNumberLike = value => +value === 0 ? true : isNumber(+value) && !Number.isNaN(+value)
 
-const isNumberLike = value => {
-  value = +value
-  return value === 0
-    ? true
-    : typeof value === 'number' && !Number.isNaN(value)
-}
+const isBaseObject = value => typeof value === 'object'
+const isBuffer = value => isBaseObject(value) && value.constructor.name === 'Buffer'
+const isObject = value => isBaseObject(value) && value.constructor.name === 'Object'
+const isUint16Array = value => isBaseObject(value) && value.BYTES_PER_ELEMENT === 2
+const isUint32Array = value => isBaseObject(value) && value.BYTES_PER_ELEMENT === 4
+const isBigInt64Array = value => isBaseObject(value) && value.BYTES_PER_ELEMENT === 8
+const isUint8Array = value => isBaseObject(value) && value.constructor.name === 'Uint8Array'
+const isUint8ClampedArray = value => isBaseObject(value) && value.constructor.name === 'Uint8ClampedArray'
+
+const isFunction = value => typeof value === 'function'
+const isGenerator = value => isFunction(value) && value.constructor.name === 'GeneratorFunction'
 
 module.exports = {
   isArray,
